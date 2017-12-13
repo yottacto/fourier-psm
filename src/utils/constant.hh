9e60c6bce728279913e5a2d7ddfd7f82d9b87d83
yottacto <yotta.w.cto@gmail.com>
2017-12-13 16:54:28 +0800

    [@] add test function, debug_f

--- src/utils/constant.hh
+++ src/utils/constant.hh
@@ -8,12 +8,24 @@ namespace fpsm
     // auto constexpr pi = boost::math::constants::pi<double>();
     auto constexpr pi = 3.14159265358979323846;
 
-    inline std::complex<double> debug_f(point const& p)
+    inline std::complex<double> default_f(point const& p)
     {
         return {
-            p.x + p.y + p.z,
-            p.x * p.y * p.z
+            std::sin(p.x) * std::sin(p.y) * std::sin(p.z),
+            std::cos(p.x) * std::cos(p.y) * std::cos(p.z)
         };
+    };
+
+    inline std::complex<double> debug_f(point const& p)
+    {
+        double s{3. + 0.25 * (
+                1. +
+                cos(2. * p.x) * cos(2. * p.y) +
+                cos(2. * p.x) * cos(2. * p.z) +
+                cos(2. * p.y) * cos(2. * p.z)
+            )
+        };
+        return s * default_f(p);
     }
 
     inline std::complex<double> test_f(point const& p)
@@ -24,12 +36,6 @@ namespace fpsm
         };
     }
 
-    inline std::complex<double> default_f(point const& p)
-    {
-        return {
-            std::sin(p.x) * std::sin(p.y) * std::sin(p.z),
-            std::cos(p.x) * std::cos(p.y) * std::cos(p.z)
-        };
-    };
+
 }
 
